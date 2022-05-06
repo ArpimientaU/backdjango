c0b91d3a740fd276208213bd42433109a4fa6bb8
Andres Pimienta <arpimienta@uniguajira.edu.co>
2022-05-06 07:17:44 -0500

    correcion en staticfile

--- pryvehiculos/pryvehiculos/settings.py
+++ pryvehiculos/pryvehiculos/settings.py
@@ -130,4 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-STATICFILES = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
